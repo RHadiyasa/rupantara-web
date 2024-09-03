5b6fe2423bc475b376084249e2441ef37947526c
Rafi Hadiyasa <rafihadiyasa32@gmail.com>
2024-09-03 20:10:34 +0700

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold">RUPANTARA</h1>
+      <Link href={"/profile"}>
+        <div className="border-2 border-black px-4 py-2 bg-slate-900 rounded-lg">
+          Profile Page
+        </div>
+      </Link>
+    </div>
+  );
+}
---
+++ src/app/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      Profile Page
+    </div>
+  );
+};
+
+export default ProfilePage;
